98deb321fb85bbf8efaf333b4e8d8f8bd918567d
Dylan <Dylan@10.0.0.178>
2017-12-10 20:03:15 -0500

    Added Correlation UI and fixed window centering

--- src/foodmood/analytics/AnalyticsNavigationUI.java
+++ src/foodmood/analytics/AnalyticsNavigationUI.java
@@ -87,6 +87,7 @@ public class AnalyticsNavigationUI extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void foodHistoryButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_foodHistoryButtonActionPerformed
